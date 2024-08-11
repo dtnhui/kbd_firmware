31e98effd331241ca4f4b3ac52e8bbb530cc9ddb
Danny Hui <dtnhui@gmail.com>
2024-08-11 12:15:30 +0800

    Add crkbd rev3

--- keyboards/crkbd/qmk/qmk_firmware/crkbd.c
+++ keyboards/crkbd/qmk/qmk_firmware/crkbd.c
@@ -1,8 +1,30 @@
+/*
+ * Copyright 2019 @foostan
+ * Copyright 2024 Danny Hui (@dtnhui)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "quantum.h"
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+#ifdef SPLIT_HAND_PIN
+    if (!is_keyboard_left()) {
+#else
     if (!is_keyboard_master()) {
+#endif
         return OLED_ROTATION_180; // flips the display 180 degrees if offhand
     }
     return rotation;
