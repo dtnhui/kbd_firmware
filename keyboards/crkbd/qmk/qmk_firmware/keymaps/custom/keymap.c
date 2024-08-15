26ec0d11a675dae03c1294cd628d4681149a263f
Danny Hui <dtnhui@gmail.com>
2024-08-15 23:27:21 +0800

    Enable key overrides feature
    - Delete: shift + backspace

--- keyboards/crkbd/qmk/qmk_firmware/keymaps/custom/keymap.c
+++ keyboards/crkbd/qmk/qmk_firmware/keymaps/custom/keymap.c
@@ -68,6 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DELETE);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
